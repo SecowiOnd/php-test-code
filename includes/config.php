1b44a58aaa5d2a2e1d3a0ab4dcbb854f8797e703
Secowi <myEmail@mail.ru>
2020-10-06 16:28:01 +0300

    Add project

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = array (
+    'title' => 'Блог IT_Минималиста',
+    'vk_url' => 'http://vk.com/somebody',
+    'db' => array(
+            'server' => 'localhost',
+            'username' => 'mysql',
+            'password' => 'mysql',
+            'name' => 'test_blog'
+        )
+);
+
+require "db.php";
